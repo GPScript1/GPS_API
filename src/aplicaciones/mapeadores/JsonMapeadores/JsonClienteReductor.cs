ab6d570e5333a8019c5811be2e73786f807ad12e
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-07-10 21:31:35 -0400

    feat: Added mappers

---
+++ src/aplicaciones/mapeadores/JsonMapeadores/JsonClienteReductor.cs
@@ -0,0 +1,18 @@
+﻿using GPScript.NET.src.aplicaciones.DTOs.jsonDTOs;
+
+namespace GPScript.NET.src.aplicaciones.mapeadores.JsonMapeadores;
+
+public class JsonClienteReductor
+{
+    public static JsonReducido Reducir(JsonEntrada jsonEntrada, string nombre)
+    {
+        return new JsonReducido
+        {
+            NombreEnte = nombre,
+            DiasInicioComFinCom = jsonEntrada.FechaEstadoFinal.DayNumber - jsonEntrada.FechaEstadoInicial.DayNumber,
+            DiasFinComInicioFactura = jsonEntrada.FechaFacturaInicial.DayNumber - jsonEntrada.FechaEstadoFinal.DayNumber,
+            DiasInicioFacturaFinPagado = jsonEntrada.FechaFacturaFinal.DayNumber - jsonEntrada.FechaFacturaInicial.DayNumber,
+            DiasInicioComFinPagado = jsonEntrada.FechaFacturaFinal.DayNumber - jsonEntrada.FechaEstadoInicial.DayNumber
+        };
+    }
+}
